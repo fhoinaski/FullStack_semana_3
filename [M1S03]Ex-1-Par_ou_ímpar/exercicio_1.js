ba096de979d6958e54dc6dba5f3ad18693fc7ed6
fhoinaski <sukinodoncai@gmail.com>
2023-02-26 19:17:21 -0300

    Exercico 1 semana 3 Par ou Impar

---
+++ [M1S03]Ex-1-Par_ou_ímpar/exercicio_1.js
@@ -0,0 +1,9 @@
+let numero=prompt("Digite um número")
+
+if (numero % 2 === 0){
+    console.log("O número digita é Par")
+    alert("O número digitado é Par")
+} else {
+    console.log("O número digitado é Ímpar")
+    alert("O número digitado é ímpar")
+}
